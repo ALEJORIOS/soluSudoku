b0b056661873da14915c6f5631578ffb9fb35579
sudoAlejandro <luisrios.lar@gmail.com>
2022-04-29 00:42:07 -0500

    .

--- SoluSudoku/Program.cs
+++ SoluSudoku/Program.cs
@@ -1,32 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SoluSudoku
 {
     class Table
     {
-        
+        private int[,] values = new int[9, 9];
+        public int[,] Values { get; set; }
         public Table()
         {
-            int[,] Values = new int[9, 9];
-        }
-        public int[,] Values { get; set; }
-
-        private string PrintTable()
-        {
-            string result = "Hola";
-            Console.Write(this.Values.Rank);
             for(int i = 0; i < 9; i++)
             {
                 for(int j = 0; j < 9; j++)
                 {
-                    Values[i, j] = 0;
+                    values[i,j] = 0;
                 }
             }
-            return result;
+            Values = values;
+        }
+        public void fillTable()
+        {
+            int value;
+            int[] coord = new int[2];
+            do
+            {
+                Console.WriteLine("Ingrese el valor");
+                value = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese la fila");
+                coord[0] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese la columna");
+                coord[1] = Convert.ToInt32(Console.ReadLine());
+                values[coord[0], coord[1]] = value;
+                Console.Clear();
+                Console.WriteLine("¿Desea ingresar otro valor? S/N");
+            } while (Console.ReadLine() == "s");
+
+        }
+        public string PrintTable()
+        {
+            var s = new StringBuilder();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    s.Append(this.values[i,j]).Append(' ');
+                }
+            s.AppendLine();
+            }
+            return s.ToString();
         }
         public override string ToString()
         {
@@ -34,13 +55,51 @@ namespace SoluSudoku
         }
 
     }
+    class TablePossibilities
+    {
+        private int[,,] values = new int[9, 9, 10];
+        public int[,,] Values { get; set; }
+
+        public TablePossibilities(Table fixedTable)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    values[i,j,0] = fixedTable.Values[i,j];
+                    for(int k = 1; k < 10; k++)
+                    {
+                        values[i,j,k] = k;
+                    }
+                }
+            }
+            Values = values;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            for(int i = 0; i < 10; i++)
+            {
+                s.Append(Values[0,0,i]);
+            }
+            return s.ToString();
+        }
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Table tablero = new Table();
             Console.Clear();
+            Table tablero = new Table();
+            TablePossibilities pruebas = new TablePossibilities(tablero);
+            //tablero.fillTable();
+            Console.WriteLine(pruebas);
             Console.Write(tablero);
+            Console.Read();
         }
 
     }
